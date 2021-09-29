21add0c2ba0647daf9972f8b78d1835b402ebbcc
Michael Steiner <michaelvsteiner@gmail.com>
2021-09-29 15:49:42 -0600

    enables put user request

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -22,7 +22,6 @@ router.get("/:id", validateUserId, (req, res, next) => {
 });
 
 router.post("/", validateUser, (req, res, next) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
   Users.insert(req.body)
     .then((user) => {
       res.status(201).json(user);
@@ -30,9 +29,12 @@ router.post("/", validateUser, (req, res, next) => {
     .catch(next);
 });
 
-router.put("/:id", validateUserId, (req, res, next) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // and another middleware to check that the request body is valid
+router.put("/:id", validateUserId, validateUser, (req, res, next) => {
+  Users.update(req.params.id, req.body)
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch(next);
 });
 
 router.delete("/:id", validateUserId, (req, res, next) => {
