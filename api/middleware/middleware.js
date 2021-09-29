abffbdf49bac7ecd218bc0954a7676670604ff54
Michael Steiner <michaelvsteiner@gmail.com>
2021-09-29 13:44:39 -0600

    adds validateUserId middleware, enables get request for all users

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,14 +1,32 @@
+const User = require("../users/users-model");
+
 function logger(req, res, next) {
   console.log(`Method:  ${req.method} \nURL:  ${req.url} \nTime:  ${new Date()}`);
   next();
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  try {
+    const existingUser = await User.getById(req.params.id);
+    if (existingUser) {
+      req.user = existingUser;
+      next();
+    } else {
+      next({ status: 404, message: "user not found" });
+    }
+  } catch (error) {
+    next(error);
+  }
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  // const { name } = req.body;
+  // if (!name || typeof name !== "string") {
+  //   next({ status: 422, message: "name is required" });
+  // } else {
+  //   next();
+  // }
 }
 
 function validatePost(req, res, next) {
--- api/server.js
+++ api/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const { logger } = require("./middleware/middleware");
+const helmet = require("helmet");
+const usersRouter = require("./users/users-router.js");
+const { logger } = require("./middleware/middleware.js");
 
 const server = express();
 server.use(express.json());
+server.use(helmet());
 
 // global middleware and the user's router need to be connected here
-server.use(logger);
+server.use("/api/users", logger, usersRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
